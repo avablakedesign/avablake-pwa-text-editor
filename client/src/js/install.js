86421b75d0570e068c4627d500e1b267c4ad389c
Ava Blake <avablakedesign@protonmail.com>
2023-01-25 15:07:31 -0500

    Added event listeners for install prompt in install.js.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let promptLater = null;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    promptLater = event;
+    butInstall.classList.remove('hidden');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (promptLater) {
+        promptLater.prompt()
+    
+        butInstall.classList.add("hidden");
+        promptLater = null;    
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    promptLater = null;
+    console.log("app installed");
+});
