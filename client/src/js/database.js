d707e3265accf7cf708ef8e968b4c30839de150a
Ava Blake <avablakedesign@protonmail.com>
2023-01-25 14:57:36 -0500

    Added index.db funtionality.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const indexDB = await openDB('jate', 1);
+  const obj = indexDB.transaction('jate', 'readwrite');
+  const savedObj = obj.objectStore('jate');
+  const saveRequest = savedObj.put({id: 1, value: content});
+  await saveRequest;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const indexDB = await openDB('jate', 1);
+  const obj = indexDB.transaction('jate', 'readonly');
+  const savedObj = obj.objectStore('jate');
+  const saveRequest = savedObj.get(1);
+  const saveResult = await saveRequest;
+  if(saveResult) {
+    return saveResult.value;
+  }
 
+}
 initdb();
